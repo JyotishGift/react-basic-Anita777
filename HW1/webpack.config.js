081a3a4a292b4d11c0aa905fe9bd785600190258
Kolupaeva Anna <131313anna84@gmail.com>
2017-08-14 22:40:51 +0300

    loader woff

--- HW1/webpack.config.js
+++ HW1/webpack.config.js
@@ -21,13 +21,10 @@ module.exports = {
           test: /\.css/,
           use: ['style-loader', 'css-loader']
         },
-        {
-          test: /\.(png|jpg|gif|svg|eot|ttf)$/,
-          loader: 'file-loader',
-          options: {
-          limit: 10000
+        { 
+          test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
+          loader: 'url-loader?limit=100000' 
         }
-      },
       ]
     },
 
